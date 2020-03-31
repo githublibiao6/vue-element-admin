8881653411d1cb44bb41938f8e3b6bc93a55e8df
李标 <1446977718@qq.com>
2020-03-31 23:23:46 +0800

    分页显示，具体其他功能没有

--- src/views/system/menu.vue
+++ src/views/system/menu.vue
@@ -35,25 +35,40 @@
       style="width: 100%;"
       @sort-change="sortChange"
     >
-      <el-table-column :label="$t('table.id')" prop="id" sortable="custom" align="center" width="80" :class-name="getSortClass('id')">
+      <el-table-column :label="$t('主键')" prop="id" sortable="custom" align="center" width="180" :class-name="getSortClass('id')">
         <template slot-scope="{row}">
-          <span>{{ row.id }}</span>
+          <span>{{ row.menuId }}</span>
         </template>
       </el-table-column>
-      <el-table-column :label="$t('table.date')" width="150px" align="center">
+      <!-- <el-table-column :label="$t('table.date')" width="150px" align="center">
         <template slot-scope="{row}">
           <span>{{ row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.title')" min-width="150px">
+      </el-table-column> -->
+      <!-- <el-table-column :label="$t('table.title')" min-width="150px">
         <template slot-scope="{row}">
           <span class="link-type" @click="handleUpdate(row)">{{ row.title }}</span>
           <el-tag>{{ row.type | typeFilter }}</el-tag>
         </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.author')" width="110px" align="center">
+      </el-table-column> -->
+      <el-table-column :label="$t('路径')" width="220px" align="center">
         <template slot-scope="{row}">
-          <span>{{ row.author }}</span>
+          <span>{{ row.url }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :label="$t('菜单名称')" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.menuText }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :label="$t('菜单类型')" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.menuType }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column :label="$t('图标')" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.icon }}</span>
         </template>
       </el-table-column>
       <el-table-column v-if="showReviewer" :label="$t('table.reviewer')" width="110px" align="center">
@@ -61,25 +76,25 @@
           <span style="color:red;">{{ row.reviewer }}</span>
         </template>
       </el-table-column>
-      <el-table-column :label="$t('table.importance')" width="80px">
+      <!-- <el-table-column :label="$t('table.importance')" width="80px">
         <template slot-scope="{row}">
           <svg-icon v-for="n in +row.importance" :key="n" icon-class="star" class="meta-item__icon" />
         </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.readings')" align="center" width="95">
+      </el-table-column> -->
+      <!-- <el-table-column :label="$t('table.readings')" align="center" width="95">
         <template slot-scope="{row}">
           <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
           <span v-else>0</span>
         </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.status')" class-name="status-col" width="100">
+      </el-table-column> -->
+      <!-- <el-table-column :label="$t('table.status')" class-name="status-col" width="100">
         <template slot-scope="{row}">
           <el-tag :type="row.status | statusFilter">
             {{ row.status }}
           </el-tag>
         </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
+      </el-table-column> -->
+      <el-table-column :label="$t('table.actions')" align="center" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
           <el-button type="primary" size="mini" @click="handleUpdate(row)">
             {{ $t('table.edit') }}
